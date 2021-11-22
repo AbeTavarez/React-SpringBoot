1c6e135dea05a2ac761fefd224970d7cf1cb0e88
abeTavarez <abeincwireless@gmail.com>
2021-11-21 21:48:00 -0500

    first components

---
+++ todo-app/src/components/learning-examples/SecondComponent.js
@@ -0,0 +1,9 @@
+const SecondComponent = () => {
+    return(
+        <div>
+            <h2>Second Component</h2>
+        </div>
+    )
+}
+
+export default SecondComponent;
---
+++ todo-app/src/components/learning-examples/ThirdComponent.js
@@ -0,0 +1,10 @@
+
+const ThirdComponent = () => {
+    return(
+        <div>
+            <h3>Third Component</h3>
+        </div>
+    )
+};
+
+export default ThirdComponent;
