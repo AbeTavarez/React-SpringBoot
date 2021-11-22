70482799d4208b33f8c487cd5830d3c0b9473b34
abeTavarez <abeincwireless@gmail.com>
2021-11-21 22:19:04 -0500

    counter

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import FirstComponent from './components/learning-examples/FirstComponent';
 import SecondComponent from './components/learning-examples/SecondComponent';
 import ThirdComponent from './components/learning-examples/ThirdComponent';
-
-
+import Counter from './components/counter/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TODO APP</h1>
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
+        <h1>Counter App</h1>
+        <Counter />
       </header>
 
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>App</h1>
+//       <FirstComponent />
+//       <SecondComponent />
+//       <ThirdComponent />
+//       </header>
+
+//     </div>
+//   );
+// }
 
 
 
---
+++ todo-app/src/components/counter/Counter.js
@@ -0,0 +1,12 @@
+import './Counter.css';
+
+const Counter = () => {
+    return (
+        <div className="container">
+            <span>0</span>
+            <button className="btn btn-primary">+1</button>
+        </div>
+    )
+}
+
+export default Counter;
