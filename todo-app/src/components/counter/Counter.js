3c5cf1cd14c9d218cee96778f4de6eb5ce400e77
abeTavarez <abeincwireless@gmail.com>
2021-11-22 23:19:14 -0500

    incrementing

--- todo-app/src/components/counter/Counter.js
+++ todo-app/src/components/counter/Counter.js
@@ -1,10 +1,18 @@
-import './Counter.css';
+import {useState} from 'react';
+
 
 const Counter = () => {
+    let [counter, setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(counter += 1)
+    }
+
     return (
         <div className="container">
+            <h1>{counter}</h1>
             <span className="mx-3">0</span>
-            <button className="btn btn-primary ml-2">+1</button>
+            <button className="btn btn-primary ml-2" onClick={increment}>+1</button>
         </div>
     )
 }
